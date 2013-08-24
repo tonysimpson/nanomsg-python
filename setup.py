0e2d9882ebb33f1a1d46a1f4d22436dfb2bb7416
Hiroki Noda <kubo39@gmail.com>
2013-08-25 04:49:50 +0900

    fix setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,21 @@
-from setuptools import setup, Extension
+try:
+    from setuptools import setup, Extension
+except ImportError:
+    from distutils.core import setup, Extension
 
-module1 = Extension('_nanomsg',
-                    sources = ['_nanomsg_cpy/wrapper.c'],
-                    libraries=['nanomsg'])
 
-setup (name = 'nanomsg',
-       version = '0.1a1',
-       packages=['nanomsg'],
-       ext_modules = [module1])
+library_dirs = ["/usr/local/lib"]
+include_dirs = ["/usr/local/include/nanomsg"]
+
+module1 = Extension('nanomsg._nanomsg',
+                    sources=['_nanomsg_cpy/wrapper.c'],
+                    libraries=['nanomsg'],
+                    include_dirs=include_dirs,
+                    library_dirs=library_dirs
+                    )
+
+
+setup(name='nanomsg',
+      version='0.1a1',
+      packages=['nanomsg'],
+      ext_modules=[module1])
