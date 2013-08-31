4ed81bb24635d1b3b0ecd51d3c396502e459c4ec
Tony Simpson <agjasimpson@gmail.com>
2013-08-31 14:19:32 +0100

    Message tests.

--- tests/test_pair.py
+++ tests/test_pair.py
@@ -25,6 +25,30 @@ class TestPairSockets(unittest.TestCase):
                 recieved = s1.recv()
         self.assertEqual(sent, recieved)
 
+    def test_send_recv_with_embeded_nulls(self):
+        with Socket(PAIR) as s1:
+            with Socket(PAIR) as s2:
+                s1.bind(SOCKET_ADDRESS)
+                s2.connect(SOCKET_ADDRESS)
+
+                sent = b'ABC\x00DEFEDDSS'
+                s2.send(sent)
+                recieved = s1.recv()
+        self.assertEqual(sent, recieved)
+
+    def test_send_recv_large_message(self):
+        with Socket(PAIR) as s1:
+            with Socket(PAIR) as s2:
+                s1.bind(SOCKET_ADDRESS)
+                s2.connect(SOCKET_ADDRESS)
+
+                sent = b'B'*(1024*1024)
+                s2.send(sent)
+                recieved = s1.recv()
+        self.assertEqual(sent, recieved)
+
+
+
 
 if __name__ == '__main__':
     unittest.main()
