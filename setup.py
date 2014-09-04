15b14a9158abfae5ae89b4e918a36eb44df166ee
Oystein Homelien <oystein@homelien.no>
2014-09-04 13:53:50 +0200

    pkgconfig: dont produce error when pkgconfig does not find .pc file

--- setup.py
+++ setup.py
@@ -18,7 +18,10 @@ with open(os.path.join('nanomsg','version.py')) as f:
 import commands
 def pkgconfig(*packages, **kw):
     flag_map = {'-I': 'include_dirs', '-L': 'library_dirs', '-l': 'libraries'}
-    for token in commands.getoutput("pkg-config --libs --cflags %s" % ' '.join(packages)).split():
+    output = commands.getoutput("pkg-config --libs --cflags %s" % ' '.join(packages))
+    if "not found" in output:
+        return {} # probably forgot to set PKG_CONFIG_PATH
+    for token in output.split():
         kw.setdefault(flag_map.get(token[:2]), []).append(token[2:])
     return kw
 
