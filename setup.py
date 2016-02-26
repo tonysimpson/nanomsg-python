2793a1c921e13631d391462fb69848a12586b31d
maple <maple.hl@gmail.com>
2016-02-26 17:55:39 +0800

    On FreeBSD, nanomsg is installed with /usr/local prefix

--- setup.py
+++ setup.py
@@ -50,6 +50,7 @@ except OSError:
     cpy_extension = Extension(str('_nanomsg_cpy'),
                         sources=[str('_nanomsg_cpy/wrapper.c')],
                         libraries=[str('nanomsg')],
+                        include_dirs=['/usr/local/include', ],
                         )
 else:
     # Building with nanoconfig
@@ -57,6 +58,7 @@ else:
                         define_macros=[('WITH_NANOCONFIG', '1')],
                         sources=[str('_nanomsg_cpy/wrapper.c')],
                         libraries=[str('nanomsg'), str('nanoconfig')],
+                        include_dirs=['/usr/local/include', ],
                         )
 install_requires = []
 
