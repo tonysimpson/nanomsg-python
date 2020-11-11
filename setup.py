465458e38a009f9fa71180b93dfc84bf047dbeab
Stephen L Arnold <nerdboy@gentoo.org>
2020-11-11 10:42:15 -0800

    update python versions, remove pep8 from test deps
    Signed-off-by: Stephen L Arnold <nerdboy@gentoo.org>

--- setup.py
+++ setup.py
@@ -76,6 +76,8 @@ setup(
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ],
     author='Tony Simpson',
     author_email='agjasimpson@gmail.com',
